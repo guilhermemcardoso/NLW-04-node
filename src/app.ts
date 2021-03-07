d9daef9e2cdb87a6a16695a2d510a19d2fd486ab
Guilherme Cardoso <guilhermemunizcardoso@gmail.com>
2021-03-07 16:57:48 -0300

    Configured swagger

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,43 @@ import createConnection from './database';
 import { router } from './routes';
 import { AppError } from './errors/AppError';
 
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
 createConnection();
 const app = express();
 
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'NLW 04 - NPS Calculation API',
+            description: 'This is a REST API application made with Express and Typescript to calculate a survey NPS.',
+            license: {
+                name: 'Licensed Under MIT',
+                url: 'https://spdx.org/licenses/MIT.html',
+            },
+            contact: {
+                name: 'Guilherme Cardoso',
+                url: 'https://www.gmcardoso.me',
+            },
+            version: '0.0.1',
+        },
+        basePath: '/v1',
+        schemes: ['HTTP']
+    },
+    apis: ['./src/routes.ts']
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json());
 app.use(router);
 app.use((error: Error, request: Request, response: Response, _next: NextFunction) => {
-    if(error instanceof AppError) {
-        return response.status(error.statusCode).json({message: error.message});
+    if (error instanceof AppError) {
+        return response.status(error.statusCode).json({ message: error.message });
     }
 
     return response.status(500).json({
@@ -21,4 +50,4 @@ app.use((error: Error, request: Request, response: Response, _next: NextFunction
     })
 })
 
-export {app};
+export { app };
